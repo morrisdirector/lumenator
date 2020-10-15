a8c6a73b3fd99009eaede6aa433831cd3aee644c
Patrick Morris <pmorris@amobee.com>
2020-10-15 08:58:51 -0400

    Version 1 WIP

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # lumenator
+Add e1.31 streaming support to your ESP8266 based light bulbs
---
+++ data/www/settings.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const OnOff = { ON: 1, OFF: 0 };
+const Mode = { STANDBY: 0, RGB: 1, WHITE: 2, GPIO_TESTING: 3 };
+
+// Debugging Mode
+const DEBUG = false;
+
+// Hello
