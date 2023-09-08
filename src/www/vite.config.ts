0319d13d69e6dfa71ccf98d13d1aa2fbb4b6efcf
Patrick Morris <pmorris@liveintent.com>
2023-09-08 14:46:13 -0400

    platformio setup

---
+++ src/www/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    outDir: '../../build',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        name: 'lumenator',
+        assetFileNames: 'lumenator.[ext]',
+        entryFileNames: 'lumenator.js'
+      }
+    }
+  }
+})
